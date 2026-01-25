a48733318c0c0af118c5b2c4fa3447e95899a2c0
jcmeve <jcmeve@naver.com>
2026-01-25 14:48:43 +0900

    add ignore

--- vcpkg_installed/x64-windows-static-md/vcpkg/blds/directxtk/x64-windows-static-md-dbg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/CM/source/repos/DX11/vcpkg_installed/x64-windows-static-md/vcpkg/blds/directxtk/src/oct2025-d895e80ef0.clean
-# Build directory: C:/Users/CM/source/repos/DX11/vcpkg_installed/x64-windows-static-md/vcpkg/blds/directxtk/x64-windows-static-md-dbg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
